6befedf6cc5fb0de966e9965146c3a1a778b238c
Dipen Chauhan <get.dipen@gmail.com>
2022-05-31 22:29:07 -0700

    Started on DatabaseManger

---
+++ Instagram/Resources/DatabaseManager.swift
@@ -0,0 +1,21 @@
+//
+//  DatabaseManager.swift
+//  Instagram
+//
+//  Created by Dipen Chauhan on 5/31/22.
+//
+
+import FirebaseDatabase
+
+public class DatabaseManager {
+    static let shared = DatabaseManager()
+    
+    // MARK: - Public
+    public func registerNewUser(username: String, email: String, password: String) {
+        
+    }
+    
+    public func loginUser(username: String?, email: String?, password: String) {
+        
+    }
+}
