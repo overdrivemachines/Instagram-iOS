ad4bf619536c44a4dde3080e027440173fbb59d3
Dipen Chauhan <get.dipen@gmail.com>
2022-06-03 22:47:55 -0700

    displaying username, password, login and create account

--- Instagram/Controllers/Other/Onboarding/LoginViewController.swift
+++ Instagram/Controllers/Other/Onboarding/LoginViewController.swift
@@ -16,6 +16,7 @@ class LoginViewController: UIViewController {
     private let usernameEmailField: UITextField = {
         let field = UITextField()
         field.placeholder = "Email or Username..."
+        field.keyboardType = .emailAddress
         field.returnKeyType = .next
         field.leftViewMode = .always
         field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10 , height: 0))
@@ -25,13 +26,15 @@ class LoginViewController: UIViewController {
         field.layer.masksToBounds = true
         field.layer.cornerRadius = Constants.cornerRadius
         field.backgroundColor = .secondarySystemBackground
+        field.layer.borderWidth = 1.0
+        field.layer.borderColor = UIColor.secondaryLabel.cgColor
         return field
     }()
     private let passwordField: UITextField = {
         let field = UITextField()
         field.isSecureTextEntry = true
         field.placeholder = "Password"
-        field.returnKeyType = .next
+        field.returnKeyType = .continue
         field.leftViewMode = .always
         field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10 , height: 0))
         field.autocapitalizationType = .none
@@ -40,6 +43,8 @@ class LoginViewController: UIViewController {
         field.layer.masksToBounds = true
         field.layer.cornerRadius = Constants.cornerRadius
         field.backgroundColor = .secondarySystemBackground
+        field.layer.borderWidth = 1.0
+        field.layer.borderColor = UIColor.secondaryLabel.cgColor
         return field
     }()
     private let loginButton: UIButton = {
@@ -60,7 +65,6 @@ class LoginViewController: UIViewController {
     private let createAccountButton: UIButton = {
         let button = UIButton()
         button.setTitle("New User? Create Account", for: .normal)
-        
         button.setTitleColor(.label, for: .normal)
         return button
     }()
@@ -75,6 +79,10 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Inside LoginVC - viewDidLoad()")
+        // Assign Deligates
+        usernameEmailField.delegate = self
+        passwordField.delegate = self
+        
         addSubviews()
         view.backgroundColor = .systemBackground
     }
@@ -103,6 +111,18 @@ class LoginViewController: UIViewController {
         imageView.contentMode = .scaleAspectFit
         imageView.frame = CGRect(x: headerView.width/4.0, y: view.safeAreaInsets.top, width: headerView.width/2.0, height: headerView.height - view.safeAreaInsets.top)
         
+        // Username Position
+        usernameEmailField.frame = CGRect(x: 25, y: headerView.bottom + 10, width: headerView.width - 50, height: 52)
+        
+        // Password Position
+        passwordField.frame = CGRect(x: 25, y: usernameEmailField.bottom + 10, width: headerView.width - 50, height: 52)
+        
+        // Login Button Position
+        loginButton.frame = CGRect(x: 25, y: passwordField.bottom + 10, width: headerView.width - 50, height: 52)
+        
+        // Create Account Button Position
+        createAccountButton.frame = CGRect(x: 25, y: loginButton.bottom + 10, width: headerView.width - 50, height: 52)
+        
     }
     
     private func addSubviews() {
@@ -110,14 +130,33 @@ class LoginViewController: UIViewController {
         view.addSubview(usernameEmailField)
         view.addSubview(passwordField)
         view.addSubview(loginButton)
+        view.addSubview(createAccountButton)
         view.addSubview(termsButton)
         view.addSubview(privacyButton)
         view.addSubview(privacyButton)
         view.addSubview(headerView)
     }
     
-    @objc private func didTapLoginButton() { }
+    @objc private func didTapLoginButton() {
+        print("didTapLoginButton()")
+    }
     @objc private func didTapTermsButton() { }
     @objc private func didTapPrivacyButton() { }
     @objc private func didTapCreateAccountButton() { }
 }
+
+extension LoginViewController: UITextFieldDelegate {
+    // When 'return' key is pressed
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if (textField == usernameEmailField) {
+            passwordField.becomeFirstResponder()
+        }
+        else if (textField == passwordField) {
+            didTapLoginButton()
+        }
+        return true
+    }
+     
+}
+
+
